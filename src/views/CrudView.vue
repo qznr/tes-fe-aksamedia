212a7ce15a92dedbb21895307cf304f5ce90d126
Moch. Gustav Ali Rafsandjani <mgustav.penting@gmail.com>
2025-01-02 17:40:34 +0700

    simple static login

---
+++ src/views/CrudView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center">
+    Hewwow >///<
+  </div>
+  <button @click="handleLogout" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+    Logout
+  </button>
+
+</template>
+<script setup>
+import { useAuth } from '../composables/auth'
+
+const { logout } = useAuth()
+
+const handleLogout = () => {
+  logout();
+}
+
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="px-8 py-6 mt-4 text-left bg-white shadow-lg dark:bg-gray-800">
+      <h3 class="text-2xl font-bold text-gray-900 dark:text-white text-center">Login ke Aplikasi</h3>
+      <LoginForm @submit="handleLogin" />
+    </div>
+  </div>
+</template>
+  
+<script setup>
+import LoginForm from '../components/auth/LoginForm.vue';
+import { useAuth } from '../composables/auth';
+
+const { login } = useAuth();
+
+const handleLogin = ({ username, password }) => {
+login(username, password);
+};
+</script>
